864a7e77b5d1c55ac8510a4ee4dabd1afcd196fe
RAJESH GOLE <rajesh.truematrix@gmail.com>
2024-04-03 16:46:52 +0530

    "Something went wrong" error msg changed to dynamic

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -25,7 +25,7 @@ export const requestHandler = async (
       localStorage.clear(); // Clear local storage on authentication issues
       if (isBrowser) window.location.href = "/login"; // Redirect to login page
     }
-    onError("Something went wrong" || error?.response?.data?.message);
+    onError(error?.response?.data?.message || "Something went wrong");
   } finally {
     // Hide loading state if setLoading function is provided
     setLoading && setLoading(false);
