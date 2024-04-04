d9052589c7b61205d233abaa0103947ba5cd5736
RAJESH GOLE <rajesh.truematrix@gmail.com>
2024-04-04 18:14:55 +0530

    countdown timer in verify otp page

--- client/src/pages/Otp/VerifyOtp.tsx
+++ client/src/pages/Otp/VerifyOtp.tsx
@@ -1,4 +1,6 @@
-// Importing necessary components and hooks
+import React, { useState, useEffect } from 'react';
+import Countdown from 'react-countdown';
+import moment from 'moment';
 import { LockClosedIcon } from "@heroicons/react/20/solid";
 import Button from "../../components/Button";
 import Input from "../../components/Input";
@@ -8,140 +10,167 @@ import * as Yup from 'yup';
 import logoitem from "../../assets/images/full-logo.png";
 import vector from "../../assets/images/auth-img.png";
 import { LocalStorage } from "../../utils";
-import Countdown from "react-countdown";
 
 interface FormValues {
     email: any;
     otp: string;
-  }
-// Component for the Login page
-const VerifyOtp = () => {
-    // Accessing the login function from the AuthContext
-    const { verifyOtp, user } = useAuth();
-    const targetDate = Date.now() + 5*60*1000; // 5mins
-  
-    const formInititalState : FormValues = {
-        email: user?.email,
-        otp: ''
-      };
-    const otpSchema = Yup.object({
-        // email: Yup.string().email('Invalid email').required('Email is required'),
-        otp: Yup.string()
-        .matches(/^[0-9]*$/, 'Please enter only numbers')
-        .min(4, 'OTP must be exactly 4 digits')
-        .max(4, 'OTP must be exactly 4 digits')
-        .required('OTP is required'),
-      });
-      const formik = useFormik({
-        enableReinitialize: true,
-        initialValues: formInititalState,
-        validationSchema:otpSchema,
-        onSubmit: async (values) => {
-          // Handle the form submission here
-          await verifyOtp(values);
-        // console.log("otp values=>",values);
-        },
-      });
-  
-   // Renderer function to customize how the countdown looks
-  const renderer = ({ minutes, seconds, completed } : any) => {
-    if (completed) {
-      // Render something when countdown is complete
-      return <span style={{ color: '#ec2d2d', alignItems: 'center' }}>OTP Expired!</span>;
-    } else {
-      // Render a countdown component
-      return (
-        <div>
-          <span>Expires in </span>&nbsp;
-          <span>{minutes}</span> min &nbsp;
-          <span>{seconds}</span> secs
-        </div>
-      );
-    }
-  };
-
-const handleResendOtp = () => {
-        LocalStorage.clear(); // Clear local storage on logout
 }
 
-  return (
-    <>
+const VerifyOtp = () => {
+    const { verifyOtp, user } = useAuth();
+    const [isFocused, setIsFocused] = useState(false);
+    const [targetDate, setTargetDate] = useState<null | moment.Moment>(null);
+    const [hasMounted, setHasMounted] = useState(false); // Track if component has mounted
+    const formInitialValues: FormValues = {
+        email: user?.email,
+        otp: ''
+    };
 
-<div className="w-full justify-between items-stretch h-screen flex flex-shrink-0 overflow-hidden g-0 login-page" >
-      <div className="w-1/3 relative px-0 bg-success p-4 align-center">
-        <div className="logo">
-        <img src={logoitem}/>
-        </div>  
+    const otpSchema = Yup.object({
+        otp: Yup.string()
+            .matches(/^[0-9]*$/, 'Please enter only numbers')
+            .min(4, 'OTP must be exactly 4 digits')
+            .max(4, 'OTP must be exactly 4 digits')
+            .required('OTP is required'),
+    });
 
-      </div>
-      <div className="w-2/3">
-          <div className="flex justify-center items-center flex-col h-screen bg-white rounded-xl relative"> 
-            <h1 className="text-3xl font-bold">Wolfpack Chat</h1>
+    const formik = useFormik({
+        enableReinitialize: true,
+        initialValues: formInitialValues,
+        validationSchema: otpSchema,
+        onSubmit: async (values, { setSubmitting }) => {
+            await verifyOtp(values);
+            setSubmitting(false);
+            const storedUser = LocalStorage.get('user');
+            const isUserPresent = storedUser !== null;
+            if (isUserPresent) {
+                LocalStorage.remove('targetDate');
+            }
+        },
+    });
 
-            <div className="p-8 flex justify-center items-center gap-5 flex-col shadow-md rounded-2xl my-5 border-[1px] login bg-white">
-              <h1 className="inline-flex items-center text-2xl mb-4 flex-col">
-                <LockClosedIcon className="h-8 w-8 mb-2 text-success" /> OTP Verification
-              </h1>
-              <p className="inline-flex items-center text-md text-success mb-2 flex-col">
-                 Enter the OTP send to email
-              </p>
-              {/* <Button
-                // fullWidth
-                className="mt-3"
-                type="button"
-                disabled
-              >Enter the OTP send to email</Button> */}
-              {/* Input for entering the email */}
-            <form onSubmit={formik.handleSubmit} className="w-full">
+   useEffect(() => {
+    const storedUser = LocalStorage.get('user');
+    const isUserPresent = storedUser !== null;
 
-              <Input
-                type="text"
-                id="otp"
-                name="otp"
-                placeholder="Enter OTP"
-                maxLength={4}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.otp}
-              />
-              {formik.touched.otp && formik.errors.otp ? (
-                                <div className='error-msg'>{formik.errors.otp}</div>
+    // Retrieve the target date from local storage only once when the component mounts
+    if (!hasMounted) {
+        const storedTargetDate = LocalStorage.get('targetDate');
+        const parsedTargetDate = storedTargetDate ? moment(storedTargetDate) : moment().add(5, 'minutes');
+        setTargetDate(parsedTargetDate);
+        setHasMounted(true); // Set hasMounted to true after initial mount
+    }
+
+    const timer = setInterval(() => {
+        if (!isFocused && !formik.isSubmitting && !isUserPresent && targetDate !== null) {
+            const updatedTargetDate = targetDate.subtract(1, 'second');
+            setTargetDate(updatedTargetDate);
+            LocalStorage.set('targetDate', updatedTargetDate.toDate());
+        }
+    }, 1000);
+
+    return () => clearInterval(timer);
+   }, [isFocused, formik.isSubmitting, hasMounted]);
+    
+    const handleResendOtp = () => {
+        // const storedUser = LocalStorage.get('user');
+        // const isUserPresent = storedUser !== null;
+        // if (isUserPresent) {
+        //     LocalStorage.clear();
+        // }
+            LocalStorage.clear();
+
+        // Reset the target date
+        // setTargetDate(moment().add(5, 'minutes'));
+        // LocalStorage.set('targetDate', moment().add(5, 'minutes').toDate());
+    }
+
+    const handleInputFocus = () => {
+        setIsFocused(true);
+    }
+
+    const renderer = ({ minutes, seconds, completed }: any) => {
+        if (completed) {
+            return <span style={{ color: '#ec2d2d', alignItems: 'center' }}>OTP Expired!</span>;
+        } else {
+            return (
+                <div>
+                    <span>Expires in </span>&nbsp;
+                    <span>{minutes}</span> min &nbsp;
+                    <span>{seconds}</span> secs
+                </div>
+            );
+        }
+    };
+
+    return (
+        <>
+            <div className="w-full justify-between items-stretch h-screen flex flex-shrink-0 overflow-hidden g-0 login-page" >
+                <div className="w-1/3 relative px-0 bg-success p-4 align-center">
+                    <div className="logo">
+                        <img src={logoitem} />
+                    </div>
+                </div>
+                <div className="w-2/3">
+                    <div className="flex justify-center items-center flex-col h-screen bg-white rounded-xl relative">
+                        <h1 className="text-3xl font-bold">Wolfpack Chat</h1>
+                        <div className="p-8 flex justify-center items-center gap-5 flex-col shadow-md rounded-2xl my-5 border-[1px] login bg-white">
+                            <h1 className="inline-flex items-center text-2xl mb-4 flex-col">
+                                <LockClosedIcon className="h-8 w-8 mb-2 text-success" /> OTP Verification
+                            </h1>
+                            <p className="inline-flex items-center text-md text-success mb-2 flex-col">
+                                Enter the OTP sent to your email
+                            </p>
+                            <form onSubmit={formik.handleSubmit} className="w-full">
+                                <Input
+                                    type="text"
+                                    id="otp"
+                                    name="otp"
+                                    placeholder="Enter OTP"
+                                    maxLength={4}
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                    onFocus={handleInputFocus} 
+                                    value={formik.values.otp}
+                                />
+                                {formik.touched.otp && formik.errors.otp ? (
+                                    <div className='error-msg'>{formik.errors.otp}</div>
                                 ) : null}
-              <Button
-                fullWidth
-                className="mt-3"
-                type="submit"
-                disabled={!formik.dirty || formik.isSubmitting}
-              >
-                Login
-              </Button>
-              </form>
-              <div style={{display:'flex', alignItems:'center'}}>
-                <div className="online-indicator"><span className={"blink"}></span></div>&nbsp;
-                <Countdown
-                  date={targetDate}
-                  intervalDelay={0}
-                  precision={3}
-                  renderer={renderer}
-                />
-              </div>
-              {/* Link to the registration page */}
-              <small className="text-zinc-300 pl-12">
-                Don&apos;t get otp !{" "}
-                <a className="text-primary hover:underline" href="/login">
-                    <button onClick={handleResendOtp}>Resend OTP</button>
-                </a>
-              </small>
-            
+                                <Button
+                                    fullWidth
+                                    className="mt-3"
+                                    type="submit"
+                                    disabled={!formik.dirty || formik.isSubmitting}
+                                >
+                                    Login
+                                </Button>
+                            </form>
+                            <div style={{ display: 'flex', alignItems: 'center' }}>
+                                <div className="online-indicator"><span className={"blink"}></span></div>&nbsp;
+                                {targetDate !== null && (
+                                    <Countdown
+                                        date={targetDate.toDate()}
+                                        intervalDelay={0}
+                                        precision={3}
+                                        renderer={renderer}
+                                    />
+                                )}
+                            </div>
+                            <small className="text-zinc-300 pl-12">
+                                Didn&apos;t receive the OTP?{" "}
+                                <a className="text-primary hover:underline" href="/login">
+                                    <button onClick={handleResendOtp}>Resend OTP</button>
+                                </a>
+                            </small>
+                        </div>
+                        <div className="auth-img">
+                            <img src={vector} alt="" />
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="auth-img">
-              <img src={vector} alt="" />
-            </div>
-          </div>
-      </div>
-    </div>
-    </>
-  );
+        </>
+    );
 };
 
 export default VerifyOtp;
