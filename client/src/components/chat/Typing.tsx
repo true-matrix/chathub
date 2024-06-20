5986b9d241b3ee5663825e31c7ab275d9c481046
RAJESH GOLE <rajeshgole96@gmail.com>
2024-06-20 20:40:00 +0530

    loader style change

--- client/src/components/chat/Typing.tsx
+++ client/src/components/chat/Typing.tsx
@@ -4,12 +4,12 @@ const Typing = () => {
   return (
     <div
       className={classNames(
-        "p-5 rounded-3xl bg-secondary w-fit inline-flex gap-1.5"
+        "p-5 rounded-3xl bg-gray-200 bg-opacity-50 w-fit inline-flex gap-1.5"
       )}
     >
-      <span className="animation1 mx-[0.5px] h-2 w-2 bg-zinc-300 rounded-full"></span>
-      <span className="animation2 mx-[0.5px] h-2 w-2 bg-zinc-300 rounded-full"></span>
-      <span className="animation3 mx-[0.5px] h-2 w-2 bg-zinc-300 rounded-full"></span>
+      <span className="animation1 mx-[0.5px] h-2 w-2 bg-green-500 rounded-full"></span>
+      <span className="animation2 mx-[0.5px] h-2 w-2 bg-green-500 rounded-full"></span>
+      <span className="animation3 mx-[0.5px] h-2 w-2 bg-green-500 rounded-full"></span>
     </div>
   );
 };
