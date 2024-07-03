5a910c523433238b14d5c1bdd99e26572140bc87
RAJESH GOLE <rajeshgole96@gmail.com>
2024-07-03 18:49:16 +0530

    Access Denied

--- client/src/components/AccessDenied.tsx
+++ client/src/components/AccessDenied.tsx
@@ -1,4 +1,10 @@
+import { LocalStorage } from '../utils';
+
 const AccessDenied = () => {
+  const handleButtonClick = () => {
+    LocalStorage.clear();
+  };
+
   return (
     <div
       className="access-denied"
@@ -10,10 +16,17 @@ const AccessDenied = () => {
         <div className="ad-message">
           <div className="ad-symbol">🚫</div>
           <div className="ad-text">
-            <h1>Access Restricted</h1>
-            <p>Please Contact Supreme Alpha</p>
+            <h1>Account Disabled</h1>
+            <p>Please contact your account admin</p>
           </div>
         </div>
+        <a
+          href="/"
+          className="mt-4 px-4 py-2 bg-green-500 text-white rounded hover:bg-green-700"
+          onClick={handleButtonClick}
+        >
+          Go to Login
+        </a>
       </div>
     </div>
   );
